240f319eff9e6132aaa838590897f5cc97ac5a2c
teixeira001 <ricardots27@gmail.com>
2023-01-25 19:06:09 -0300

    Enviando toda a pasta do projeto umpara um da Udemy

---
+++ Relacionamento_1_para_1_Java/src/CarroTeste.java
@@ -0,0 +1,33 @@
+public class CarroTeste {
+    public static void main(String[] args) {
+        
+        Carro c1 = new Carro();
+        System.out.println(c1.estaligado());
+
+        c1.ligar();
+        System.out.println(c1.estaligado());     
+        
+        System.out.println(c1.motor.giros());
+
+        c1.acelerar();
+        c1.acelerar();
+        c1.acelerar();
+        c1.acelerar();
+
+        System.out.println(c1.motor.giros());
+        
+        c1.frear();
+        c1.frear();
+        c1.frear();
+        c1.frear();
+       
+
+        //Faltou o encapsulamento !!!
+        //c1.motor.fatorInterjeiicao  =-30;
+        System.out.println(c1.motor.giros());
+
+        //Relação bidimencional
+        System.out.println(c1.motor.carro.motor.carro.motor.giros());
+    }
+    
+}
