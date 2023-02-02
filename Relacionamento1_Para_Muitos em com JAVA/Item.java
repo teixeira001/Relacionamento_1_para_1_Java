6b3d4f022616ae7a906b1e691b13856e157aeca4
teixeira001 <ricardots27@gmail.com>
2023-02-02 18:21:28 -0300

    Exercicio 02 de java da Udemy

---
+++ Relacionamento1_Para_Muitos em com JAVA/Item.java
@@ -0,0 +1,16 @@
+package Relacionamento1_Para_Muitos;
+
+public class Item {
+    String nome;
+    int quantidade;
+    double preco;
+
+
+    Item(String nome,int quantidade, double preco) {
+        this.nome= nome;
+        this.quantidade = quantidade;
+        this.preco = preco;
+        
+    }
+    
+}
